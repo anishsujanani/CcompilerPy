b6abe912ac7b61e500742d5ea6dbe11934e39798
Anish Sujanani <ansujanani@gmail.com>
2020-07-28 14:04:19 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,6 @@
 
 
 # class that creates the symbol table from the tokens
-# do I need to take care of escape sequences?
 class tokenizer:
 	def __init__(self):
 		self.token_stream = []
@@ -31,11 +30,9 @@ class tokenizer:
 		string_state = False
 		self.token_stream = token_stream
 		buf = ''
-		#print self.token_stream
 		pop_list = []
 		for i in range(0, len(self.token_stream)):
 			if self.token_stream[i] == 'include' and self.token_stream[i+1] == '<':
-				#include <stdio.h>
 				if '.' in self.token_stream[i+3] and 'h' in self.token_stream[i+4]:
 					libstrbuf = self.token_stream[i+2] + self.token_stream[i+3] + self.token_stream[i+4]
 					self.token_stream[i+2] = libstrbuf
@@ -45,12 +42,8 @@ class tokenizer:
 		while 'DEL' in self.token_stream:
 			self.token_stream.remove('DEL')
 
-		print 'HEREHERE', self.token_stream
 
 		for i in self.token_stream:
-
-			
-			
 			if i == '"' and string_state == False:
 				string_state = True
 				self.symbol_table.append({'token_type': 'punctuation', 'value': '"'})
@@ -58,7 +51,6 @@ class tokenizer:
 			elif i == '"' and string_state == True:
 				# create " token and add 'buf' as string literal token
 				string_state = False
-				#print buf
 				self.symbol_table.append({'token_type': 'const', 'value': buf})
 				self.symbol_table.append({'token_type': 'punctuation', 'value': '"'})
 				buf = ''
@@ -66,10 +58,7 @@ class tokenizer:
 
 			if string_state == True:
 				buf += i
-
-			
-
-
+				
 			elif i in self.keywords:
 				self.symbol_table.append( {'token_type': 'keyword', 'value': i} )
 
@@ -112,5 +101,4 @@ class tokenizer:
 					
 			
 	
-		#self.symbol_table.append(token_stream)
 		return self.symbol_table	
